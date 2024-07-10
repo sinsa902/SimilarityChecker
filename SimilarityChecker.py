ccca98f73b767fed11968c643761105f625bc0b0
gyu.byeon <isoul902@gmail.com>
2024-07-10 16:15:00 +0900

    [UPDATE] step1: calc_len 구현 중

--- SimilarityChecker.py
+++ SimilarityChecker.py
@@ -1,2 +1,31 @@
 class SimilarityChecker:
-    pass
+    def __init__(self):
+        self.char_score = 0
+        self.len_score = 0
+        self.score = 0
+
+    def getScore(self):
+        return self.score
+
+    def run(self, param):
+        self._set_inputs(param)
+        self._calc_and_update_len_score()
+        self._calc_and_update_char_score()
+        self._update_total_score()
+
+    def _calc_and_update_len_score(self):
+        if len(self.input1) == len(self.input2):
+            self.len_score = 60
+        else:
+            self.len_score = 0
+
+    def _calc_and_update_char_score(self):
+        self.char_score = 40
+
+    def _update_total_score(self):
+        self.score = 0
+        self.score = self.len_score + self.char_score
+
+    def _set_inputs(self, param):
+        self.input1 = param[0]
+        self.input2 = param[1]
--- test_SimilarityChecker.py
+++ test_SimilarityChecker.py
@@ -1,6 +1,17 @@
 from unittest import TestCase
 
+from SimilarityChecker import SimilarityChecker
+
 
 class Test(TestCase):
+    def setUp(self) -> None:
+        self.sc = SimilarityChecker()
+        super().setUp()
+
     def test_similarity_checker(self):
-        self.fail()
+        self.sc.run(["ABCDE", "ABCDE"])
+        self.assertEqual(100, self.sc.getScore())
+
+    def test_similarity_checker2(self):
+        self.sc.run(["ABCDE", "FGHIJ"])
+        self.assertEqual(100, self.sc.getScore())
