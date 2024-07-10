65d26d2db0749fc421b2c581a383e2beb8f70409
gyu.byeon <isoul902@gmail.com>
2024-07-10 16:27:44 +0900

    [UPDATE] step2: calc_len 길이 체크 구현 완료. invalid도 추가

--- SimilarityChecker.py
+++ SimilarityChecker.py
@@ -14,18 +14,28 @@ class SimilarityChecker:
         self._update_total_score()
 
     def _calc_and_update_len_score(self):
-        if len(self.input1) == len(self.input2):
-            self.len_score = 60
-        else:
+        max_len = max(len(self.input1), len(self.input2))
+        min_len = min(len(self.input1), len(self.input2))
+
+        if max_len >= min_len * 2:
             self.len_score = 0
+            self.invalid = True
+            return
+
+        gap = max_len - min_len
+        self.len_score = (1 - gap / min_len) * 60
 
     def _calc_and_update_char_score(self):
         self.char_score = 40
 
     def _update_total_score(self):
         self.score = 0
+        if self.invalid:
+            self.score = 0
+            return
         self.score = self.len_score + self.char_score
 
     def _set_inputs(self, param):
+        self.invalid = False
         self.input1 = param[0]
         self.input2 = param[1]
--- test_SimilarityChecker.py
+++ test_SimilarityChecker.py
@@ -15,3 +15,11 @@ class Test(TestCase):
     def test_similarity_checker2(self):
         self.sc.run(["ABCDE", "FGHIJ"])
         self.assertEqual(100, self.sc.getScore())
+
+    def test_similarity_checker3(self):
+        self.sc.run(["ABCDE", "FGH"])
+        self.assertEqual(60, self.sc.getScore())
+
+    def test_similarity_checker4(self):
+        self.sc.run(["ABCDE", "F"])
+        self.assertEqual(0, self.sc.getScore())
